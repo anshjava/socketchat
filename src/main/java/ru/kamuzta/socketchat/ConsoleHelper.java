83bd7853c3fc8a59bdee3ac5ff21723bc6503a1d
Andrey <ansh.msk@gmail.com>
2021-05-07 12:40:45 +0300

    ver1.0

---
+++ src/main/java/ru/kamuzta/socketchat/ConsoleHelper.java
@@ -0,0 +1,37 @@
+package ru.kamuzta.socketchat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        while (true) {
+            String inputString;
+            try {
+                inputString = br.readLine();
+                return inputString;
+            } catch (IOException ioe) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            String inputString;
+            try {
+                inputString = readString();
+                return Integer.parseInt(inputString);
+            } catch (NumberFormatException nfe) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/kamuzta/socketchat/Server.java
@@ -0,0 +1,127 @@
+package ru.kamuzta.socketchat;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        System.out.print("Wellcome to chatServer\nEnter port number: ");
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+
+            ConsoleHelper.writeMessage("Server started");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                new Handler(clientSocket).start();
+
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Map.Entry<String, Connection> entry : connectionMap.entrySet()) {
+            try {
+                entry.getValue().send(message);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    private static class Handler extends Thread {
+        Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            ConsoleHelper.writeMessage("Установлено новое соединение с " + socket.getRemoteSocketAddress());
+
+            String userName = null;
+
+            try (Connection connection = new Connection(socket)) {
+                userName = serverHandshake(connection);
+
+                // Сообщаем всем участникам, что присоединился новый участник
+                sendBroadcastMessage(new Message(MessageType.USER_ADDED, userName));
+
+                // Сообщаем новому участнику о существующих участниках
+                notifyUsers(connection, userName);
+
+                // Обрабатываем сообщения пользователей
+                serverMainLoop(connection, userName);
+
+            } catch (IOException | ClassNotFoundException e) {
+                ConsoleHelper.writeMessage("Ошибка при обмене данными с " + socket.getRemoteSocketAddress());
+            }
+
+            if (userName != null) {
+                connectionMap.remove(userName);
+                sendBroadcastMessage(new Message(MessageType.USER_REMOVED, userName));
+            }
+
+            ConsoleHelper.writeMessage("Соединение с " + socket.getRemoteSocketAddress() + " закрыто.");
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            String userName;
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+
+                Message message = connection.receive();
+                if (message.getType() != MessageType.USER_NAME) {
+                    ConsoleHelper.writeMessage("Получено сообщение от " + socket.getRemoteSocketAddress() + ". Тип сообщения не соответствует протоколу.");
+                    continue;
+                } else {
+                    userName = message.getData();
+                }
+
+
+                if (userName.isEmpty()) {
+                    ConsoleHelper.writeMessage("Попытка подключения к серверу с пустым именем от " + socket.getRemoteSocketAddress());
+                    continue;
+                } else if (connectionMap.containsKey(userName)) {
+                    ConsoleHelper.writeMessage("Попытка подключения к серверу с уже используемым именем от " + socket.getRemoteSocketAddress());
+                    continue;
+                } else {
+                    connectionMap.put(userName, connection);
+                    connection.send(new Message(MessageType.NAME_ACCEPTED));
+                }
+                return userName;
+            }
+        }
+
+        private void notifyUsers(Connection connection, String userName) throws IOException {
+            for (String name : connectionMap.keySet()) {
+                if (name.equals(userName))
+                    continue;
+                connection.send(new Message(MessageType.USER_ADDED, name));
+            }
+        }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+            while (true) {
+                Message msg = connection.receive();
+
+                if (msg.getType() != MessageType.TEXT) {
+                    ConsoleHelper.writeMessage("Error, the message type is not TEXT");
+                } else {
+                    String messageToSend = userName + ": " + msg.getData();
+                    sendBroadcastMessage(new Message(MessageType.TEXT, messageToSend));
+                }
+            }
+
+        }
+    }
+}
